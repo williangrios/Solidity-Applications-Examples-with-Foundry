559f82224b2c5056d950bc4bdf631483e6b2e4f4
willian <williangrios@yahoo.com.br>
2024-02-29 14:52:08 -0300

    refactor: remove useless code

--- test/EtherWallet.t.sol
+++ test/EtherWallet.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Test, console2} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {EtherWallet} from "../src/EtherWallet.sol";
 
 contract EtherWalletTests is Test {
